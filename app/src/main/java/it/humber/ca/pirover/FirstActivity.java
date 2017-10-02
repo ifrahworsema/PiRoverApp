c2ca7bac6bc09933f8d85d78e35af2ced9f9bada
chris0707 <31682624+chris0707@users.noreply.github.com>
2017-10-02 19:03:53 -0400

    added PiRover menu and login layout

--- app/src/main/java/it/humber/ca/pirover/FirstActivity.java
+++ app/src/main/java/it/humber/ca/pirover/FirstActivity.java
@@ -1,7 +1,10 @@
 package it.humber.ca.pirover;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -10,6 +13,15 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-        int b;
+        final Button loginB = (Button)findViewById(R.id.loginButton);
+        loginB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
